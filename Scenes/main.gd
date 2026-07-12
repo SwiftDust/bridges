b64e467e597d2beaaa2f954b0a6c6343d586bf25
Mart <me@m4rt.nl>
2026-07-12 20:25:45 +0200

    feat: map generation through text file

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,10 +3,17 @@ extends Node2D
 
 @onready var tile_map = $TileMapLayer
 
-var level_1 = {} # TODO: load this in via a text file
+var level_arr: PackedStringArray = []
+
 
 func _ready() -> void:
-	tile_map.draw_map([Vector2(0, 0), Vector2(1, 1), Vector2(2, 2)])
+	var levels_dir = ResourceLoader.list_directory("res://Levels")
+	for level_dir in levels_dir:
+		var level_file = FileAccess.open("res://Levels/" + level_dir + "/level.txt", FileAccess.READ).get_as_text()
+		level_arr = level_file.strip_edges().split()
+	
+	
+	tile_map.draw_map(level_arr)
 
 func _process(delta: float) -> void:
 	pass
--- Scenes/tile_map_layer.gd
+++ Scenes/tile_map_layer.gd
@@ -1,12 +1,15 @@
 extends TileMapLayer
 
+
 @export var GRID_SIZE_X = 9
 @export var GRID_SIZE_Y = 5
 
+
+
 func draw_map(islands: Array):
-	for x in GRID_SIZE_X:
-		for y in GRID_SIZE_Y:
-			if islands.has(Vector2(x, y)):
-				set_cell(Vector2i(x, y), 1, Vector2i(0,0), 0) # draw island
-			else:
-				set_cell(Vector2i(x, y), 0, Vector2i(0,0), 0) # draw sea
+	for i in range(0, len(islands)):
+		print(Vector2i(i / GRID_SIZE_Y, i % GRID_SIZE_Y))
+		if islands[i] != "0":
+			set_cell(Vector2i(i / GRID_SIZE_Y, i % GRID_SIZE_Y), 1, Vector2i(0,0), 0) # draw island
+		else:
+			set_cell(Vector2i(i / GRID_SIZE_Y, i % GRID_SIZE_Y), 0, Vector2i(0,0), 0) # draw sea
